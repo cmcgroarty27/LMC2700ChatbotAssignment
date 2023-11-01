0a30074b134d0303eaa5cd6d3dbbd6542520c557
arhea9 <123708712+arhea9@users.noreply.github.com>
2023-11-01 15:10:17 -0400

    Update openai-test.js
    Add Doozey personality, add .message.content to console log line as seen in demo

--- openai-test.js
+++ openai-test.js
@@ -8,11 +8,11 @@ const openai = new OpenAI({ apiKey:'sk-8cZkH8RrsLqDcJpq9xD4T3BlbkFJ6mXHTFqWU7PWd
 
 async function main() {
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    messages: [{ role: "system", content: "You are a friendly, conversational, funny assistant." }],
     model: "gpt-3.5-turbo",
   });
 
-  console.log(completion.choices[0]);
+  console.log(completion.choices[0].message.content);
 }
 
 main();
